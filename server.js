bcd99995e3f7c9f42f58273a35aac932f4b4ee01
Iheb KHEMISSI <iheb.khemissi+github@gmail.com>
2013-08-13 10:38:20 -0400

    switching to ExpressJS (initial basic setup)

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(process.env.PORT, process.env.IP);
+var express = require("express");
+var http = require("http");
+var app = express();
+
+app.all("*", function(request, response, next) {
+  response.writeHead(200, { "Content-Type": "text/plain" });
+  next();
+});
+
+app.get("/", function(request, response) {
+  response.end("Welcome to the homepage!");
+});
+
+app.get("/about", function(request, response) {
+  response.end("Welcome to the about page!");
+});
+
+app.get("*", function(request, response) {
+  response.end("404!");
+});
+
+http.createServer(app).listen(process.env.PORT, process.env.IP);

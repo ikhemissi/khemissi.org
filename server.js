530d773cfcd39e62b0869b4440c8e4c1847e3d52
Iheb KHEMISSI <iheb.khemissi+github@gmail.com>
2013-08-13 11:18:43 -0400

    using ejs as a templating engine

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ var express = require("express");
 var http = require("http");
 var app = express();
 
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+/*
 app.all("*", function(request, response, next) {
   response.writeHead(200, { "Content-Type": "text/plain" });
   next();
 });
+*/
 
 app.get("/", function(request, response) {
-  response.end("Welcome to the homepage!");
+  //response.end("Welcome to the homepage!");
+  response.render('index')
 });
 
 app.get("/about", function(request, response) {
